3240f135640d54836822294752cd7f3ff5b74eba
WindAsMe <542636539@qq.com>
2018-09-10 16:42:36 +0800

    Web Crawler in Java

--- src/com/company/WebCrawlerDFS.java
+++ src/com/company/WebCrawlerDFS.java
@@ -8,34 +8,39 @@ import org.jsoup.select.Elements;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.*;
 
 
 /**
  * Author     : WindAsMe
- * File       : WebCrawler.java
+ * File       : WebCrawlerDFS.java
  * Time       : Create on 18-9-10
- * Location   : ../Home/Crawler/WebCrawler.java
- * Function   : Web Crawler
+ * Location   : ../Home/Crawler/WebCrawlerDFS.java
+ * Function   : Web Crawler in DFS
  */
-public class WebCrawler {
+public class WebCrawlerDFS {
 
-    // Save the url which is marked.
+    // Save the url which is marked
     private static Set<String> set = new HashSet<>();
     private static int count = 0;
     // DFS searching web
-    private static void craweler(String url) {
+    private static void craweler(String url, Connection c) {
         if (set.size() > 9 || set.contains(url))
             return;
         try {
             // Save the correlative url
+            // In this step, every url is working
             Queue<String> queue = new ArrayDeque<>();
             Document doc = Jsoup.connect(url).get();
+            Statement statement = c.createStatement();
             count++;
             // mark
             set.add(url);
             System.out.println("\nTitle: " + doc.title() + " URL: " + url + " Running times: " + count);
-
+            System.out.println("set: " + set.toString());
             // Element a including [href] attribute
             Elements links = doc.select("a[href]");
             Elements media = doc.select("[src]");
@@ -62,30 +67,27 @@ public class WebCrawler {
                 System.out.println((" * " + link.attr("abs:href") + trim(link.text(), 35)));
 
                 // Sort the valid url
-                try {
-                    if (s.length() > 4 && s.substring(0, 4).equals("http")) {
-                        URL u = new URL(s);
-                        HttpURLConnection connection = (HttpURLConnection) u.openConnection();
-                        int state = connection.getResponseCode();
-                        if (state == 200)
-                            queue.add(s);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                if (validURL(s))
+                    queue.add(s);
+            }
+
+            if (!c.isClosed()) {
+                System.out.println("Insert INTO dfs_table (url, media, imports, links) VALUES (" + "\"" + url + "\"" + "," + media.size() + "," + imports.size() + "," + links.size() + ")");
+                statement.execute("Insert INTO dfs_table (url, media, imports, links) VALUES (" + "\"" + url + "\"" + "," + media.size() + "," + imports.size() + "," + links.size() + ")");
             }
 
             // Waiting 2.5s
             Thread.sleep(2500);
 
             while (!queue.isEmpty())
-                craweler(queue.poll());
+                craweler(queue.poll(), c);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // trim the url
     private static String trim(String s, int width) {
         if (s.length() > width)
             return s.substring(0, width - 1) + ".";
@@ -93,11 +95,39 @@ public class WebCrawler {
             return s;
     }
 
+    // 1. http(s) is the start
+    // 2. Can be connected
+    private static boolean validURL(String s) {
+        // if s.length < 4, first piece is in utility
+        if (s.length() > 10 && s.substring(0, 4).equals("http")) {
+            try {
+                URL url = new URL(s);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                return connection.getResponseCode() == 200;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else
+            return false;
+    }
+
     public static void main(String[] args) {
-        // java.util.Scanner input = new java.util.Scanner(System.in);
-        // System.out.print("Enter a URL: ");
-        // String url = input.nextLine();
-        craweler("https://www.baidu.com");
+        try {
+            // java.util.Scanner input = new java.util.Scanner(System.in);
+            // System.out.print("Enter a URL: ");
+            // String url = input.nextLine();
+            String driver = "com.mysql.jdbc.Driver";
+            String username = "root";
+            String password = "change123";
+            String database = "jdbc:mysql://localhost:3306/IRDB";
+            Class.forName(driver);
+            Connection c = DriverManager.getConnection(database, username, password);
+            craweler("https://www.baidu.com", c);
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
